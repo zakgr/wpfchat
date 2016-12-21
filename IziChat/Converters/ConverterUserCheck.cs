575fc273f9b30e9a7095256732f43f69eef40d43
zakgr <zakcorepower@hotmail.com>
2016-12-21 18:01:49 +0200

    Add Converters: UserCheck , ConvertOnechar in Folder Converters

---
+++ IziChat/Converters/ConverterUserCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace IziChat.Converters
+{
+    public class ConverterUserCheck : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length!=3) throw new Exception("2 Usernames Expected");
+            return values[0].ToString()==values[1].ToString()+ values[2].ToString() ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
