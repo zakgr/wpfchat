037355595c3a961c391434f9ac477c5009142363
Portno <j.remoundos@dotbydot.gr>
2016-12-22 11:54:16 +0200

    added shadow on mainwindow

--- ChatLib/ChatServer.cs
+++ ChatLib/ChatServer.cs
@@ -42,7 +42,7 @@ namespace ChatLib
                 Task.Run(async () =>
                 {
                     await Task.Delay(500);
-                    for (int i = 0; i < 100000; i++)
+                    for (int i = 0; i < 1000; i++)
                     {
                         Broadcast(new BroadcastMessage()
                         {
